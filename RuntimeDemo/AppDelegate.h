b9bb7e2bf6dfe0896e655a912500309eb8ebdc10
sifang <lsif1202@163.com>
2016-03-10 16:33:20 +0800

    Initial Commit

---
+++ RuntimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeDemo
+//
+//  Created by AAA on 16/3/10.
+//  Copyright © 2016年 easywed.cn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
